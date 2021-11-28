a596ba45c4ff8b4bcda82368f684d9a676486495
Hugo Berard <hberard@fb.com>
2021-11-28 18:13:30 -0500

    Processing a bunch of files

--- demo.py
+++ demo.py
@@ -6,6 +6,7 @@ import sys
 import argparse
 import cv2
 import yaml
+import glob
 
 from FaceBoxes import FaceBoxes
 from TDDFA import TDDFA
@@ -39,55 +40,57 @@ def main(args):
         tddfa = TDDFA(gpu_mode=gpu_mode, **cfg)
         face_boxes = FaceBoxes()
 
-    # Given a still image path and load to BGR channel
-    img = cv2.imread(args.img_fp)
+    list_files = sorted(glob.glob(args.img_fp))
+    for img_fp in list_files:
+        # Given a still image path and load to BGR channel
+        img = cv2.imread(img_fp)
 
-    # Detect faces, get 3DMM params and roi boxes
-    boxes = face_boxes(img)
-    n = len(boxes)
-    if n == 0:
-        print(f'No face detected, exit')
-        sys.exit(-1)
-    print(f'Detect {n} faces')
+        # Detect faces, get 3DMM params and roi boxes
+        boxes = face_boxes(img)
+        n = len(boxes)
+        if n == 0:
+            print(f'No face detected, exit')
+            sys.exit(-1)
+        print(f'Detect {n} faces')
 
-    param_lst, roi_box_lst = tddfa(img, boxes)
+        param_lst, roi_box_lst = tddfa(img, boxes)
 
-    # Visualization and serialization
-    dense_flag = args.opt in ('2d_dense', '3d', 'depth', 'pncc', 'uv_tex', 'ply', 'obj')
-    old_suffix = get_suffix(args.img_fp)
-    new_suffix = f'.{args.opt}' if args.opt in ('ply', 'obj') else '.jpg'
+        # Visualization and serialization
+        dense_flag = args.opt in ('2d_dense', '3d', 'depth', 'pncc', 'uv_tex', 'ply', 'obj')
+        old_suffix = get_suffix(img_fp)
+        new_suffix = f'.{args.opt}' if args.opt in ('ply', 'obj') else '.jpg'
 
-    wfp = f'examples/results/{args.img_fp.split("/")[-1].replace(old_suffix, "")}_{args.opt}' + new_suffix
+        wfp = img_fp.replace(old_suffix, new_suffix)
 
-    ver_lst = tddfa.recon_vers(param_lst, roi_box_lst, dense_flag=dense_flag)
+        ver_lst = tddfa.recon_vers(param_lst, roi_box_lst, dense_flag=dense_flag)
 
-    if args.opt == '2d_sparse':
-        draw_landmarks(img, ver_lst, show_flag=args.show_flag, dense_flag=dense_flag, wfp=wfp)
-    elif args.opt == '2d_dense':
-        draw_landmarks(img, ver_lst, show_flag=args.show_flag, dense_flag=dense_flag, wfp=wfp)
-    elif args.opt == '3d':
-        render(img, ver_lst, tddfa.tri, alpha=0.6, show_flag=args.show_flag, wfp=wfp)
-    elif args.opt == 'depth':
-        # if `with_bf_flag` is False, the background is black
-        depth(img, ver_lst, tddfa.tri, show_flag=args.show_flag, wfp=wfp, with_bg_flag=True)
-    elif args.opt == 'pncc':
-        pncc(img, ver_lst, tddfa.tri, show_flag=args.show_flag, wfp=wfp, with_bg_flag=True)
-    elif args.opt == 'uv_tex':
-        uv_tex(img, ver_lst, tddfa.tri, show_flag=args.show_flag, wfp=wfp)
-    elif args.opt == 'pose':
-        viz_pose(img, param_lst, ver_lst, show_flag=args.show_flag, wfp=wfp)
-    elif args.opt == 'ply':
-        ser_to_ply(ver_lst, tddfa.tri, height=img.shape[0], wfp=wfp)
-    elif args.opt == 'obj':
-        ser_to_obj(img, ver_lst, tddfa.tri, height=img.shape[0], wfp=wfp)
-    else:
-        raise ValueError(f'Unknown opt {args.opt}')
+        if args.opt == '2d_sparse':
+            draw_landmarks(img, ver_lst, show_flag=args.show_flag, dense_flag=dense_flag, wfp=wfp)
+        elif args.opt == '2d_dense':
+            draw_landmarks(img, ver_lst, show_flag=args.show_flag, dense_flag=dense_flag, wfp=wfp)
+        elif args.opt == '3d':
+            render(img, ver_lst, tddfa.tri, alpha=0.6, show_flag=args.show_flag, wfp=wfp)
+        elif args.opt == 'depth':
+            # if `with_bf_flag` is False, the background is black
+            depth(img, ver_lst, tddfa.tri, show_flag=args.show_flag, wfp=wfp, with_bg_flag=True)
+        elif args.opt == 'pncc':
+            pncc(img, ver_lst, tddfa.tri, show_flag=args.show_flag, wfp=wfp, with_bg_flag=True)
+        elif args.opt == 'uv_tex':
+            uv_tex(img, ver_lst, tddfa.tri, show_flag=args.show_flag, wfp=wfp)
+        elif args.opt == 'pose':
+            viz_pose(img, param_lst, ver_lst, show_flag=args.show_flag, wfp=wfp)
+        elif args.opt == 'ply':
+            ser_to_ply(ver_lst, tddfa.tri, height=img.shape[0], wfp=wfp)
+        elif args.opt == 'obj':
+            ser_to_obj(img, ver_lst, tddfa.tri, height=img.shape[0], wfp=wfp)
+        else:
+            raise ValueError(f'Unknown opt {args.opt}')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='The demo of still image of 3DDFA_V2')
     parser.add_argument('-c', '--config', type=str, default='configs/mb1_120x120.yml')
-    parser.add_argument('-f', '--img_fp', type=str, default='examples/inputs/trump_hillary.jpg')
+    parser.add_argument('-f', '--img_fp', type=str, default=None)
     parser.add_argument('-m', '--mode', type=str, default='cpu', help='gpu or cpu mode')
     parser.add_argument('-o', '--opt', type=str, default='2d_sparse',
                         choices=['2d_sparse', '2d_dense', '3d', 'depth', 'pncc', 'uv_tex', 'pose', 'ply', 'obj'])
